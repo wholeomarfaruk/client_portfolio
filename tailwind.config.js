1d9755e88fec7a59db8639ecfaf8636911705be6
wholeomarfaruk <wholeomarfaruk@gmail.com>
2023-06-11 00:18:44 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+    colors: {
+      'grayblues': '#3D405B',
+    },
+  },
+  plugins: [],
+}
+
